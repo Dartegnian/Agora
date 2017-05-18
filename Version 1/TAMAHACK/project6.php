2d47ff661cac00d8a2c5a77c45e7b5533bc9905c
Dartegnian <dartegnian@users.noreply.github.com>
2017-05-18 14:18:45 +0800

    Code changes

--- Version 1/TAMAHACK/project6.php
+++ Version 1/TAMAHACK/project6.php
@@ -18,9 +18,6 @@
 		});
 		function Redirect() {
                window.location="registration.php";
-               //window.location="https://google.com";
-               //window.location.replace("registration.php");
-
         }
 	</script>
 	<script type="text/javascript">
@@ -58,19 +55,10 @@
       }
     </script>
     <script type="text/javascript">
-
-      // Load the Visualization API and the corechart package.
       google.charts.load('current', {'packages':['corechart']});
-
-      // Set a callback to run when the Google Visualization API is loaded.
       google.charts.setOnLoadCallback(drawChart);
 
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
       function drawChart() {
-
-        // Create the data table.
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'Names');
         data.addColumn('number', 'Amountt');
@@ -81,14 +69,9 @@
           ['Miscellaneous', 100000],
 		  ['Labor', 50000000]
         ]);
-
-        // Set chart options
         var options = {'title':'Pozo Negro Maintenance Budget Breakdown in Phillipine Peso (PHP)',
                        'width':900,
                        'height':800};
-
-
-        // Instantiate and draw our chart, passing in some options.
         var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
         chart.draw(data, options);
       }
