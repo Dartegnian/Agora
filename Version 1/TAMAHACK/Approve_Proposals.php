25501035ab3e9e3ccc92f18fc6f79ea09a25bdff
Dartegnian <dartegnian@users.noreply.github.com>
2017-05-18 14:10:06 +0800

    Code changes

--- Version 1/TAMAHACK/Approve_Proposals.php
+++ Version 1/TAMAHACK/Approve_Proposals.php
@@ -6,43 +6,36 @@
 
 	<body>
 		<?php
-		$username = $_GET['username'];
-		echo "Hello ".$username."!!!";
-		
-		
-			$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];	
-					
-			// create short variable names
-				
-			$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];	
-			$proposals = file("$DOCUMENT_ROOT/Agora/Proposals.txt");
-			$number_of_proposals = count($proposals);
-			$outputstring="";
-			// echo $number_of_products;
+			$username = $_GET['username'];
+			echo "Hello ".$username."!!!";
+				$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
+				$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];	
+				$proposals = file("$DOCUMENT_ROOT/Agora/Proposals.txt");
+				$number_of_proposals = count($proposals);
+				$outputstring="";
 
-			for ($i=0;$i<$number_of_proposals;$i++) {
-				$proposals_col = explode("~",$proposals[$i]);
-				for ($j=0;$j<6;$j++) {
-					$list[$i][$j] = $proposals_col[$j];
-					}
-				}
-			
-			for ($x=0;$x<$number_of_proposals;$x++) {
-				if(isset($_POST[$x])){
-					for($y=0;$y<6;$y++){
-						if ($y==5){
-						$outputstring .= $list[$x][$y];							
-						}else{
-						$outputstring .= $list[$x][$y].'~';
+				for ($i=0;$i<$number_of_proposals;$i++) {
+					$proposals_col = explode("~",$proposals[$i]);
+					for ($j=0;$j<6;$j++) {
+						$list[$i][$j] = $proposals_col[$j];
 						}
 					}
-					$fp = fopen("$DOCUMENT_ROOT/Agora/Approved_Proposals.txt", "at");
-					fwrite($fp, $outputstring);
-					$outputstring='';
-				}
-			}			
-			
-			?>
+				
+				for ($x=0;$x<$number_of_proposals;$x++) {
+					if(isset($_POST[$x])){
+						for($y=0;$y<6;$y++){
+							if ($y==5){
+							$outputstring .= $list[$x][$y];							
+							}else{
+							$outputstring .= $list[$x][$y].'~';
+							}
+						}
+						$fp = fopen("$DOCUMENT_ROOT/Agora/Approved_Proposals.txt", "at");
+						fwrite($fp, $outputstring);
+						$outputstring='';
+					}
+				}				
+		?>
 	<h1>Congratulations, you have successfully approved the chosen proposals.</h1>
 	<br/><br/><br/>
 	<p>
