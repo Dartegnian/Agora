098301448f8b139da353792ab980e176d905a5db
Dartegnian Velarde <Dartegnian@users.noreply.github.com>
2017-06-19 23:19:01 +0800

    Minor changes

--- Version 1/TAMAHACK/project3.php
+++ Version 1/TAMAHACK/project3.php
@@ -80,7 +80,7 @@
 </head>
 <body>
 	<div id="fullwidth" style="height: 50px;">
-		<h1 style="display: inline;"><a href="http://localhost/TAMAHACK"><img id="logo" src="Resources\Images\agora.png"\></a></h1>
+		<h1 style="display: inline;"><a href="http://localhost/TAMAHACK"><img id="logo" src="Resources\Images\agora.png"/></a></h1>
 	</div>
 	<header id="mproj3">
 		<h1 class="slogan"><span class="rotate">GOBYERNONG PROYEKTO, PARA SA PILIPINO, PARA SA PANGKALAHATAN, AGORA</span></h1>
@@ -90,7 +90,7 @@
 		<div id="imagecontainer">
 			<div id="image1container" class="images">
 				<a href="Resources\Images\CArticle3.jpg" target='_blank'>
-					<img src="Resources\Images\CArticle3.jpg"\>
+					<img src="Resources\Images\CArticle3.jpg"/>
 				</a>
 			</div>
 		</div>
@@ -120,7 +120,7 @@
 	</div>
 	<footer>
 		<a href="Resources\Images\coatofarms.png" target='_blank'>
-			<img src="Resources\Images\coatofarms.png"\>
+			<img src="Resources\Images\coatofarms.png"/>
 		</a>
 		<div class="footcontain">
 			<h4>About GOV.PH</h4>
